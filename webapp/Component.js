1c8b263e17a6e8d8191e5b77c8e7b71d29721154
MustafaTahirAkdemir <themtahir@gmail.com>
2024-10-08 18:13:56 +0300

    Resource Models

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,16 +2,10 @@ sap.ui.define([
 	"sap/ui/core/UIComponent"
 ], (UIComponent) => {
 	"use strict";
-
 	return UIComponent.extend("ui5.databinding.Component", {
 		metadata: {
 			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
-		},
-		init(){
-			UIComponent.prototype.init.apply(this, arguments);
-			
-			this.getModel().setDefaultBindingMode(BindingMode.OneWay);
 		}
 	});
 });
